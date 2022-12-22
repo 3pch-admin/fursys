fa9a927a97ef666078f74a0f865da7d792853bb0
JungNed <ned9912@naver.com>
2022-12-22 16:45:38 +0900

    메롱

--- fursys/src/platform/util/service/CPCHistoryHelper.java
+++ fursys/src/platform/util/service/CPCHistoryHelper.java
@@ -3,6 +3,7 @@ package platform.util.service;
 import platform.util.entity.CPCHistory;
 import wt.fc.PersistenceHelper;
 import wt.method.RemoteAccess;
+import wt.pom.Transaction;
 import wt.services.ServiceFactory;
 
 public class CPCHistoryHelper implements RemoteAccess {
@@ -15,13 +16,28 @@ public class CPCHistoryHelper implements RemoteAccess {
 	
 	public static void createCPCHistory(String oid, String module, String sendQuery) throws Exception {
 		
-		CPCHistory hi = CPCHistory.newCPCHistory();
-		hi.setTargetOid(oid);
-		hi.setModuleName(module);
-		hi.setSendQuery(sendQuery);
-		
-		PersistenceHelper.manager.save(hi);
-		
+		Transaction trs = new Transaction();
+		try {
+			trs.start();
+			
+			CPCHistory hi = CPCHistory.newCPCHistory();
+			hi.setTargetOid(oid);
+			hi.setModuleName(module);
+			hi.setSendQuery(sendQuery);
+			
+			PersistenceHelper.manager.save(hi);
+			
+			trs.commit();
+			trs = null;
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			trs.rollback();
+			throw e;
+		} finally {
+			if (trs != null)
+				trs.rollback();
+		}
 	}
 	
 	
